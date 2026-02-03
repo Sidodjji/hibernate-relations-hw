8319d8f5b5c10ddb66a739925aa8efba58508839
Sidodji <andreymaluk35@gmail.com>
2026-02-04 01:39:03 +0200

    Hibernate relations impl

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,22 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create SessionFactory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return SESSION_FACTORY;
     }
 }
