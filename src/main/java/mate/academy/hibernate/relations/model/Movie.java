d92c4487dd2c8c1e6ff585efeaa0f53125e94748
Sidodji <andreymaluk35@gmail.com>
2026-02-04 01:47:23 +0200

    Fix imports

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,12 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Movie implements Cloneable {
